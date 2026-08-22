91f64269ee8ed33f076e91386978770c54950109
bywinsty <bywinsty@users.noreply.github.com>
2026-08-22 19:52:46 +0200

    test: match quoted account_id warning SQL

--- tests/test_database_migration.cpp
+++ tests/test_database_migration.cpp
@@ -35,7 +35,9 @@ int main()
     assert(plan.archiveConflicts.size() == 2);
     assert(plan.removeConflicts.size() == 2);
     assert(plan.normalizeLegacy.find("4294967296") != std::string::npos);
-    assert(plan.warnUnmapped.find("account_id = 0") != std::string::npos);
+    assert(plan.warnUnmapped.find("`account_id` = 0") != std::string::npos);
+    assert(plan.warnUnmapped.find("`account_id` > 4294967295") != std::string::npos);
+    assert(plan.warnUnmapped.find("`account_id` > 76561202255233023") != std::string::npos);
     assert(plan.finalizeUnsignedColumn.find("BIGINT UNSIGNED") != std::string::npos);
     assert(plan.recordVersion.find("steamid64-v2") != std::string::npos);
     return 0;
